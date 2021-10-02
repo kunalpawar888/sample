c0efa789c97e647073c40984f8fe347f8b337715
kunalpawar888 <pawarkunal173@gmail.com>
2021-10-02 23:22:05 +0530

    my first code

---
+++ src/Addition.scala
@@ -0,0 +1,20 @@
+object Addition {
+  def main(args: Array[String]) {
+
+
+
+    println("Enter the first number : ");
+
+    // reading input from user
+    var a = 10;
+
+    println("Enter the second number : ");
+
+    // reading input from user
+    var b = 20;
+
+    println("The Result is : "+(a+b));
+
+  }
+
+}
